cc0931a7485d02485dfc5f1d134a8d123244d4f5
Aut1sta <godoyguilherme799@gmail.com>
2021-08-11 20:52:11 -0300

    Making all functional

--- js/script.js
+++ js/script.js
@@ -5,42 +5,93 @@ let computerScore = 0;
 let roundWinner = ''
 
 let computerSelection = computerPlay();
-
-function computerPlay(){
-    let randomNumber = parseInt(Math.random() + 2);
-    switch(randomNumber){
-        case 0:
-            return 'ROCK'
-            break;
-        case 1: 
-            return 'PAPER'
-            break;
-        case 2:
-            return 'SCISSOR'
-            break;
-    }
-}
+let playerSelection = '';
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection == computerSelection){
-        roundWinner = 'Draw!';
+    if (playerSelection === computerSelection){
+        roundWinner = 'draw';
+        updateScore()
     }
-    if((playerSelection == 'ROCK' && computerSelection == 'SCISSOR')|| 
-       (playerSelection == 'PAPER' && computerSelection == 'ROCK' )||
-       (playerSelection == 'SCISSOR' && computerSlection == 'PAPER')){
-           playerScore++
-           roundWinner = 'Player'
+    if ((playerSelection === 'ROCK' && computerSelection === 'SCISSOR')|| 
+       (playerSelection === 'PAPER' && computerSelection === 'ROCK')||
+       (playerSelection === 'SCISSOR' && computerSelection === 'PAPER'))
+    {
+        playerScore++;
+        roundWinner = 'player';
+        updateScore()
     }
-    if((playerSelection == 'ROCK' && computerSelection == 'PAPER')|| 
-       (playerSelection == 'PAPER' && computerSelection == 'SCISSOR' )||
-       (playerSelection == 'SCISSOR' && computerSlection == 'ROCK')){
-        computerScore++
-        roundWinner = 'Computer'
- }
+    if ((playerSelection === 'ROCK' && computerSelection === 'PAPER')|| 
+       (playerSelection === 'PAPER' && computerSelection === 'SCISSOR')||
+       (playerSelection === 'SCISSOR' && computerSelection === 'ROCK'))
+    {
+        computerScore++;
+        roundWinner = 'computer';
+        updateScore()
+    }
 }
 
+function computerPlay(){
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+      case 0:
+        return 'ROCK'
+      case 1:
+        return 'PAPER'
+      case 2:
+        return 'SCISSOR'
+    }
+}
 
+function isGameOver(playerScore, computerScore){
+    if (playerScore === 5){
+        scorePara.innerHTML = 'Player Win the Game'
+        restartGame()
+    }else if(computerScore === 5){
+        scorePara.innerHTML = 'Computer Win the Game'
+        restartGame()
+    } 
+}
 
+function updateChoice(){
+    computerSelection = computerPlay();
+}
+
+function restartGame(){
+    playerScore = '';
+    playerScore = 0;
+    computerScore = 0;
+
+    playerScorePara.innerHTML = `PlayerScore: ${playerScore}`;
+    computerScorePara.innerHTML = `ComputerScore: ${computerScore}`
+}
+
+//UI
+
+const playerScorePara = document.getElementById("playerScore");
+const computerScorePara = document.getElementById("computerScore");
+const scorePara = document.getElementById("scoreResults")
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorBtn = document.getElementById("scissorBtn")
+
+function updateScore(){
+    switch(roundWinner){
+        case 'draw':
+            scorePara.innerHTML = `Draw! \n player: ${playerSelection} \n computer: ${computerSelection}`;
+            break;
+        case 'player':
+            scorePara.innerHTML = `Player Win! \n player: ${playerSelection} \n computer: ${computerSelection}`;
+            break;
+        case 'computer':
+            scorePara.innerHTML = `Computer Win! \n player: ${playerSelection} \n computer: ${computerSelection}`;
+            break;
+    }
+
+    playerScorePara.innerHTML = `PlayerScore: ${playerScore}`;
+    computerScorePara.innerHTML = `ComputerScore: ${computerScore}`
+    updateChoice()
+    isGameOver(playerScore, computerScore)
+}
 
 
 
